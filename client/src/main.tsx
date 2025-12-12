bbaabc991fdb673884950ca284698711a566be71
HafsaMudassar <hafsamedu.pk@gmail.com>
2025-12-12 22:56:16 +0500

    s

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { AuthProvider } from "./context/AuthContext";
+
 import "./index.css";
 
 // @ts-ignore: virtual module provided by Vite PWA plugin at build time
@@ -8,8 +8,4 @@ import { registerSW } from 'virtual:pwa-register';
 
 registerSW();
 
-createRoot(document.getElementById("root")!).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
-);
+
